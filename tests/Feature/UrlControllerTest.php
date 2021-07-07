ed4d71123d438f4ca79b7cffd41adca18b7e9183
hallernsk <haller@ngs.ru>
2021-07-07 16:00:27 +0700

    test with sqlite(:memory:)

--- tests/Feature/UrlControllerTest.php
+++ tests/Feature/UrlControllerTest.php
@@ -5,14 +5,18 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
 
 class UrlControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
+
     public function testUrlsLoading()
     {
         $response = $this->get(route('urls'));
@@ -21,10 +25,20 @@ class UrlControllerTest extends TestCase
         
     }
 
+    public function testStore()
+    {
+        $data = ['name' => 'http://test.www'];
+        $this->assertDatabaseMissing('urls', $data);        
+        $response = $this->post(route('store'), $data);
+        $this->assertDatabaseHas('urls', $data);
+    }
     public function testUrlLoading()
     {
-        $response = $this->get('urls/1');
+//        $id = DB::table('urls')->select('id')->get();
+        $id = DB::table('urls')->insertGetId(['name' => 'http://test.www']);
+        $response = $this->get(route('url', $id));
 
         $response->assertOk();
     }
+
 }
