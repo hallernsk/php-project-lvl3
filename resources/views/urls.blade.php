6beb3ea086ba48f266dad8df31c247205f725ae4
hallernsk <haller@ngs.ru>
2021-08-16 11:07:08 +0700

    correct urls.blade

--- resources/views/urls.blade.php
+++ resources/views/urls.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<main class="flex-grow-1">
     @include('flash::message')
-
-    <h1 class="mt-5 mb-3">Сайты</h1>
-    <table class="table table-bordered table-hover text-nowrap">
-        <tr>
-            <th>ID</th>
-            <th>Имя</th>
-            <th>Код ответа</th>
-            <th>Последняя проверка</th>
-        </tr>
-        @foreach ($urls as $url)
+    <div class="container-lg">
+            <h1 class="mt-5 mb-3">Сайты</h1>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover text-nowrap">
+              <tr>
+                <th>ID</th>
+                <th>Имя</th>
+                <th>Код ответа</th>
+                <th>Последняя проверка</th>
+              </tr>
+            @foreach ($urls as $url)
             <tr>
                 <td>{{ $url->id }}</td>
                 <td><a href="/urls/{{$url->id}}">{{ $url->name }}</a></td>
                 <td>{{$lastCheck[$url->id]->status_code ?? ''}}</td>
                 <td>{{$lastCheck[$url->id]->created_at ?? ''}}</td>
             </tr>
-        @endforeach
-
-</div>
+            @endforeach
+            </table>
+        </div>
+    </div>
+</main>
 @endsection
