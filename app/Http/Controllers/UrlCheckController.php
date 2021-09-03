e77ec5d8430f6f6370fce9c084de307ea7dfc83c
hallernsk <haller@ngs.ru>
2021-09-03 23:49:20 +0700

    add RequestException,ConnectionException in store(UrlCheckController)

--- app/Http/Controllers/UrlCheckController.php
+++ app/Http/Controllers/UrlCheckController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
 use DiDom\Document;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\ConnectionException;
 
 class UrlCheckController extends Controller
 {
@@ -20,27 +22,30 @@ class UrlCheckController extends Controller
     {
         $url = DB::table('urls')->find($id);
         abort_unless($url, 404);
-        $response = Http::get($url->name);
+        try {
+            $response = Http::get($url->name);
 
-        $document = new Document($response->body());
-        $h1 = optional($document->first('h1'))->text();
-        $keywords = optional($document->first('meta[name=keywords]'))->getAttribute('content');
-        $description = optional($document->first('meta[name=description]'))->getAttribute('content');
-        $status = $response->status();
+            $document = new Document($response->body());
+            $h1 = optional($document->first('h1'))->text();
+            $keywords = optional($document->first('meta[name=keywords]'))->getAttribute('content');
+            $description = optional($document->first('meta[name=description]'))->getAttribute('content');
+            $status = $response->status();
 
-        DB::table('url_checks')->insert(
-            [
-                'url_id' =>  $id,
-                'h1' => $h1,
-                'keywords' => $keywords,
-                'description' => $description,
-                'status_code' => $status,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]
-        );
-        flash('Страница успешно проверена');
-
+            DB::table('url_checks')->insert(
+                [
+                    'url_id' => $id,
+                    'h1' => $h1,
+                    'keywords' => $keywords,
+                    'description' => $description,
+                    'status_code' => $status,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ]
+            );
+            flash('Страница успешно проверена');
+        } catch (RequestException | ConnectionException $e) {
+            flash($e->getMessage())->error();
+        }
         return redirect()->route('urls.show', $id);
     }
 }
