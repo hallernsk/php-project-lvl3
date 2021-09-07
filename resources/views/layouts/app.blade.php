92fe41b6cc3e4f6e1f39b13694b7ef322e2f43c6
hallernsk <haller@ngs.ru>
2021-09-07 11:23:38 +0700

    correct app.blade(indents)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Page analyzer </title>
-<!-- CSS only (bootstrap !)-->
+    <!-- CSS only (bootstrap !)-->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body class="min-vh-100 d-flex flex-column">
-<header class="flex-shrink-0">
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">Анализатор страниц</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/">Главная</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/urls">Сайты</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-</header>
-
-@if ($errors->any())
-    <div class="alert alert-danger">
+  </head>
+  <body class="min-vh-100 d-flex flex-column">
+    <header class="flex-shrink-0">
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Анализатор страниц</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" href="/">Главная</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link " href="/urls">Сайты</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    @if ($errors->any())
+      <div class="alert alert-danger">
         <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
         </ul>
-    </div>
-@endif
-@include('flash::message')
-
+      </div>
+    @endif
+    @include('flash::message')
     @yield('content')
-</body>
+  </body>
 </html>
