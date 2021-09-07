b32bbdbe2dcd40bdcef967fad168885375a1678c
hallernsk <haller@ngs.ru>
2021-09-07 12:55:31 +0700

    fixed app.blade(href-helpers)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,17 +13,17 @@
   <body class="min-vh-100 d-flex flex-column">
     <header class="flex-shrink-0">
       <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">Анализатор страниц</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Анализатор страниц</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-            <a class="nav-link active" href="/">Главная</a>
+            <a class="nav-link " href="{{ route('home') }}">Главная</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link " href="/urls">Сайты</a>
+            <a class="nav-link " href="{{ route('urls.index') }}">Сайты</a>
             </li>
           </ul>
         </div>
